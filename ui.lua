d17e6be7b46e69ec23d84c9432b94d72ee0267ad
goonersigma05-gif <goonersigma05@gmail.com>
2026-08-26 16:38:48 -0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,3 +1,3 @@
-script_key = "trall"
+script_key = "Trial"
 
 loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/8e6a498d177e0bc062d6c4b1aa14f97890dd98a49ac3c777b9290e5526ef1689.lua"))()
